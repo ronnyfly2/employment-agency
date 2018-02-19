11f751acd9bd74093015e7cc50900d3ac37c5323
Ronny Cabrera Sinrahua <ronny@mesa247.pe>
2018-02-19 18:24:31 -0500

    init nuxt

--- agency/pages/index.vue
+++ agency/pages/index.vue
@@ -1,24 +1,16 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        agency
-      </h1>
-      <h2 class="subtitle">
-        agency hecho en nuxt
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+	section.container
+		div
+			logo
+			h1.title agency
+			h2.subtitle agency hecho en nuxt
+			.links
+				a(href="https://nuxtjs.org/" target="_blank" class="button--green") Documentation
+				a(href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey") GitHub
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
-
 export default {
   components: {
     Logo
@@ -26,34 +18,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus">
 .container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+	min-height 100vh
+	display flex
+	justify-content center
+	align-items center
+	text-align center
 .title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+	display block
+	font-weight 300
+	font-size 100px
+	color #35495e
+	letter-spacing 1px
 .subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+	font-weight 300
+	font-size 42px
+	color #526488
+	word-spacing 5px
+	padding-bottom 15px
 .links
-{
-  padding-top: 15px;
-}
+	padding-top 15px
 </style>
